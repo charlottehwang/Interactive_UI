c2536cbbf840503229e8bffccfab7cde893c1973
Charlotte <charlotteehwang@gmail.com>
2025-07-23 13:38:14 -0700

    Initial Commit

---
+++ Interactive_UI/Interactive_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_UIApp.swift
+//  Interactive_UI
+//
+//  Created by Charlotte Hwang on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
